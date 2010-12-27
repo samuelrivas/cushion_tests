9bbd7a3d5e96acc19b8eef81cd9c66cbdd094870
Samuel Rivas <samuelrivas@gmail.com>
2010-12-27 16:32:13 +0100

    FUNCT added a generator for unicode characters
    I'm finding lots of issues, time to attack the root

--- ebin/cushion_tests.app
+++ ebin/cushion_tests.app
@@ -2,7 +2,7 @@
  [{description, "Tests for cushion"},
   {vsn, "devel"},
   {modules, [cushion_tests, cushion_fsm_test, cushion_couch_api_test,
-             cushion_json_test]},
+             cushion_json_test, cushion_test_gen]},
   {registered, []},
   {applications, [kernel, stdlib, cushion, eunit, eqc, tools]},
   {env, []}]}.
---
+++ src/cushion_tests_gen.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author Samuel Rivas <samuelrivas@gmail.com>
+%%% @copyright (C) 2010, Samuel Rivas
+%%% @doc Generic generators for QuickCheck tests
+%%%
+%%% @end
+%%% Created : 27 Dec 2010 by Samuel Rivas <samuelrivas@gmail.com>
+%%%-------------------------------------------------------------------
+-module(cushion_tests_gen).
+
+-export([unicode_char/0]).
+
+%%--------------------------------------------------------------------
+%% @doc Generates an unicode character
+%%
+%% This generator distributes character generation equally in several groups to
+%% avoid biasing the generation too much to the long tail of unicode characters
+%% after the most common characters and the most relevant 7-bit characters from
+%% 0 to 127. See the code for more info.
+%%
+%% Lower values are weighted more than larger values
+%%
+%% @end
+%%--------------------------------------------------------------------
+unicode_char() ->
+    eqc_gen:frequency(
+      [{5, eqc_gen:choose(0, 31)}, % There are some non printable characters here
+       {5, eqc_gen:choose(32, 126)}, % "normal" characters
+       {3, eqc_gen:choose(127, 255)},
+       {1, eqc_gen:choose(255, 16#d7ff)},  % jump over reserved 0xD800-0xDFFF
+       {1, eqc_gen:choose(16#e000, 16#fffd)}, % jump over 0xfffe and 0xffff
+       {1, eqc_gen:choose(16#10000, 16#10ffff)} % remaining range of unicode
+      ]).
